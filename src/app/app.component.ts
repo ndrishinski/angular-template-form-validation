37dfee715b81a091e261b1207ed66c0d66006ca4
Nick Drishinski <ndrishinski@yahoo.com>
2019-01-22 13:09:02 -0700

    simple template driven form with validation and bootstrap styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+import { Hero } from './hero';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'formValidate';
+
+  powers = ['Really Smart', 'Super Flexible',
+            'Super Hot', 'Weather Changer'];
+
+  model = new Hero('','','');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
+
+  newHero() {
+  // this.model = new Hero('','','');
+  console.log(this.model);
+}
 }
